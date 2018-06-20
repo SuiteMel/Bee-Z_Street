2c4964742e9e2feccb2859ccb9056efc9b589734
Jennifer Lang <jenniferlang1921@gmail.com>
2018-06-20 18:24:53 -0500

    dragndrop

--- client/src/pages/garden.js
+++ client/src/pages/garden.js
@@ -1,10 +1,46 @@
-import React from "react";
+import React, { Component } from "react";
 import "../css/home.css";
+import Plants from "../components/Plants";
+import Target from "../components/Target";
+import HTML5Backend from 'react-dnd-html5-backend'
+import {DragDropContext } from 'react-dnd'
 
-const Garden = () => (
-  <div>
-    <h2>My Beautiful Garden</h2>
-  </div>
-);
 
-export default Garden;
+
+class Garden extends Component {
+  state = {
+    plants: [
+      { id: 1, name: 'Item 1' },
+      { id: 2, name: 'Item 2' },
+      { id: 3, name: 'Item 3' },
+      { id: 4, name: 'Item 4' },
+    ],
+  }
+
+  deletePlant= (id) => {
+    this.setState(prevState => {
+      let plants = prevState.plants;
+      const index = plants.findIndex(plants => plants.id === id);
+      plants.splice(index, 1);
+      return { plants };
+    });
+  }
+
+  render() {
+    return (
+      <div className="Garden">
+      <h1 className="Garden-title">Welcome To Your Garden!</h1>
+      <div className="plant-container">
+        {this.state.plants.map((plants, index) => (
+          <Plants key={plants.id} plants={plants} handleDrop={(id) =>
+            this.deletePlant(id)}/>
+        ))}
+      </div>
+
+      <Target />
+      </div>
+  );
+  }
+}
+
+  export default DragDropContext(HTML5Backend)(Garden);
