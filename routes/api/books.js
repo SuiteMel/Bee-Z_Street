05835b1cebed07e0bcadb5f90d42f26359e7178f
Patrick Kearney <pckearney@yahoo.com>
2018-06-20 14:00:55 -0500

    no more book refs

--- routes/api/books.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router();
-const booksController = require("../../controllers/plantsController");
-
-// Matches with "/api/plants"
-router.route("/").get(booksController.findAll);
-
-module.exports = router;
