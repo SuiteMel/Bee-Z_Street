2e1f0ebadbd9bcf3e651ba55a919f807e2fa10c8
Samantha McCall <samanthaadams.biz@gmail.com>
2018-06-20 17:37:05 -0500

    adding form components

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import {BrowserRouter as Router, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 import Home from "./pages/home";
 import Search from "./pages/search";
 import Garden from "./pages/garden";
@@ -8,7 +9,7 @@ import Contact from "./pages/contact";
 import "./App.css";
 
 const App = () => (
-
+<div>
   <Router>
     <div> 
     <Navbar />
@@ -17,7 +18,12 @@ const App = () => (
     <Route exact path="/garden" component={Garden} />
     <Route exact path="/contact" component={Contact} />
     </div>
+    
   </Router>
+  <Footer> 
+  <p>Bee-Z Street &copy; 2018</p>
+</Footer>
+</div>
 );
 
 export default App;
--- client/src/pages/contact.js
+++ client/src/pages/contact.js
@@ -1,10 +1,54 @@
-import React from "react";
+import React, { Component } from 'react';
 import "../css/home.css";
+import Form from "../components/Form";
+import Button from "../components/Button";
+import Wrapper from "../components/Wrapper";
 
-const Contact = () => (
-  <div>
-    <h2>I'm gonna get ahold of you!</h2>
-  </div>
-);
+class Contact extends Component {
+  constructor (props){
+    super(props)
+  }
+  state = {
+    topic: ""
+  }
+  handleInputChange=(e) =>{
+    console.log(e)
+    this.setState({
+      topic: e.target.value
+    })
+  }
+  handleSubmit=() =>{
+    console.log(this.state.topic)
+    // axios.post(`/test`, { topic:this.state.message })
+    .then(res => {
+      console.log(res);
+      console.log(res.data);
+    })
+  }
+  //invoked to load component data
+  componentDidMount() {
+  }
+
+  render() {
+    return (
+      <div>
+        <Wrapper>
+              <h2>Contact Bee-Z Street</h2>
+              <Form 
+                handleInputChange={this.handleInputChange}
+                {...this.props}
+                value={this.state.message}
+              />
+              <Button 
+                className="submit" 
+                onClick={this.handleSubmit}
+              >
+                Search
+              </Button>          
+        </Wrapper>
+      </div>
+    );
+  }
+}
 
 export default Contact;
