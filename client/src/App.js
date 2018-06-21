b8204b303d9bc55e5bab118e5bc4a2d0ee118bec
Samantha McCall <samanthaadams.biz@gmail.com>
2018-06-21 16:10:17 -0500

    materializing contact

--- client/src/App.js
+++ client/src/App.js
@@ -6,22 +6,26 @@ import Home from "./pages/home";
 import Search from "./pages/search";
 import Garden from "./pages/garden";
 import Contact from "./pages/contact";
+import Wrapper from "./components/Wrapper";
 import "./App.css";
 
 const App = () => (
 <div>
-  <Router>
-    <div> 
-    <Navbar />
-    <Route exact path="/" component={Home} />
-    <Route exact path="/search" component={Search} />
-    <Route exact path="/garden" component={Garden} />
-    <Route exact path="/contact" component={Contact} />
-    </div>   
-  </Router>
-  <Footer> 
-  <h3>Bee-Z Street &copy; 2018</h3>
-</Footer>
+  <Wrapper>
+    <Router>
+      <div> 
+      <Navbar />
+      <Route exact path="/" component={Home} />
+      <Route exact path="/search" component={Search} />
+      <Route exact path="/garden" component={Garden} />
+      <Route exact path="/contact" component={Contact} />
+      </div>   
+    </Router>
+    </Wrapper>
+    <Footer> 
+    <h3>Bee-Z Street &copy; 2018</h3>
+  </Footer>
+
 </div>
 );
 
--- client/src/pages/contact.js
+++ client/src/pages/contact.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import "../css/home.css";
 import { Button, Input, TextArea } from "../components/Form";
-import Wrapper from "../components/Wrapper";
 
 class Contact extends Component {
   state = {
@@ -13,44 +12,77 @@ class Contact extends Component {
   render() {
     return (
     <div>
-  <Wrapper >
-
     <form class="form" action="mailto:someone@example.com" method="post" enctype="text/plain">
     <h1>Bee-friendly gardening is a breeze!</h1>
     <h2>Have a question? Let us help!</h2>
     <h3>Subject</h3>
-      <Input
-        type="text"
-        value={this.state.subject.text}
-        onChange={this.handleInputChange}
-        name="subject"
-        placeholder="Ex.: Plant identification"
-      />
+    <div class="row">
+    <div class="col s12">
+      <div class="row">
+        <div className="input-field col s3 black-text">
+          {/* <i class="material-icons prefix">textsms</i> */}
+          <input 
+          type="text" 
+          className="black-text"
+          value={this.state.subject.text}
+          onChange={this.handleInputChange}
+          name="subject"
+          placeholder="Ex.: Plant identification"
+          />
+        </div>
+      </div>
+    </div>
+    {/* new field */}
     <h3>Post your question</h3>
-
-      <TextArea 
-        value={this.state.body.text}
-        onChange={this.handleInputChange}
-        type="text"
-        name="body"
-        placeholder="Leave your message here. If you're asking about a specific plant 
-        and you have a picture, upload it below."
-      />
-      <h3>Select a file (.pdf, .jpg .png or .gif):</h3> 
-      <Input
+    <div class="col s12">
+      <div class="row">
+        <div className="input-field col s3 black-text">
+          {/* <i class="material-icons prefix">textsms</i> */}
+          <TextArea 
+          value={this.state.body.text}
+          onChange={this.handleInputChange}
+          type="text"
+          class="black-text" 
+          name="body"
+          placeholder="Leave your message here. If you're asking about a specific plant 
+          and you have a picture, upload it below."
+          />
+        </div>
+      </div>
+    </div>
+    {/* new field */}
+    <h3>Select a file (.pdf, .jpg .png or .gif):</h3> 
+    <div class="col s12">
+      <div class="row">
+        <div className="input-field col s3 black-text">
+          {/* <i class="material-icons prefix">textsms</i> */}
+          <Input
         type="file"
         onChange={this.handleInputChange}
         name="image"
       />
-      <Button
-        type="submit"
-        value="Submit"
-        onClick={this.handleFormSubmit}
-      >
-        Submit
-    </Button>
-    </form>
-  </Wrapper >
+        </div>
+      </div>
+    </div>
+        {/* new field */}
+    <div class="col s12">
+      <div class="row">
+        <div className="input-field col s3 black-text">
+          {/* <i class="material-icons prefix">textsms</i> */}
+          <Button
+            class="btn waves-effect waves-light #f9a825 yellow darken-3" 
+            name="action"
+            type="submit"
+            value="Submit"
+            onClick={this.handleFormSubmit}
+          >
+              Submit
+          </Button>
+        </div>
+      </div>
+    </div>
+  </div>
+</form>
 </div >
     )};
   };
