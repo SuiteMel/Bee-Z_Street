e7f5d093c1ff4a8be54b2a07110eb0b6e65ec8d6
Samantha McCall <samanthaadams.biz@gmail.com>
2018-06-22 12:38:17 -0500

    aligned margins with header/footer on garden and contact

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -5,23 +5,32 @@ import Main from "../components/Main";
 
 const Home = () => (
   <Main>
-    <h2>Welcome to bee-friendly gardening!</h2>
+    <div class="container">
+      <div class="row">
+        <div class="col s4">
+          sign up/login fields here
+    </div>
+        <div class="col s8">
+          <h2>Welcome to bee-friendly gardening!</h2>
 
-    <p>If you're like many homeowners today, you're interested in beautifying your 
-      garden with fewer chemicals and more diverse plants. The great news is it's never 
+          <p>If you're like many homeowners today, you're interested in beautifying your
+            garden with fewer chemicals and more diverse plants. The great news is it's never
       been easier to introduce hearty, native plants into your own yard.</p>
 
-    <p>If you're planning garden updates, you probably know pollinators play an important 
-      role in the health and beauty of the plants you choose. Honey bees around the world are 
-      dying off at alarming rates, but you can help them by choosing indigenous plants. Since these 
+          <p>If you're planning garden updates, you probably know pollinators play an important
+            role in the health and beauty of the plants you choose. Honey bees around the world are
+            dying off at alarming rates, but you can help them by choosing indigenous plants. Since these
       plants grow in your region anyway, they're great natural food sources for bees.</p>
 
-    <p>It doesn't take any extra work on your part to help bees and other pollinators. In fact, 
-      native plants use less water and are more climate tolerant. The fascinating bees, butterflies, 
+          <p>It doesn't take any extra work on your part to help bees and other pollinators. In fact,
+            native plants use less water and are more climate tolerant. The fascinating bees, butterflies,
       and birds they attract are a beautiful bonus Mother Nature throws in free of charge!</p>
 
-    <p>Bee-Z Street is designed to help you choose easy-care native plants that brighten and beautify 
+          <p>Bee-Z Street is designed to help you choose easy-care native plants that brighten and beautify
       your home landscaping. We think you'll love the results; the bees will thank you too!</p>
+        </div>
+      </div>
+    </div>
   </Main>
 );
 
