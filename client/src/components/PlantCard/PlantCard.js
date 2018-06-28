1f583e64dc0270b16dc74af06def4249299211c3
Melissa Waltner <melissamwaltner@gmail.com>
2018-06-28 15:05:57 -0500

    Added search function and created modals on click

--- client/src/components/PlantCard/PlantCard.js
+++ client/src/components/PlantCard/PlantCard.js
@@ -2,13 +2,17 @@ import React from "react";
 // import "./Main.css";
 
 const PlantCard = props => (
-  <li className="collection-item avatar">
+  <li className="collection-item avatar" id={props.id}>
     <img src={props.image} alt="" className="circle" />
-      <span className="title">{props.commonName}</span> <small><em>{props.name}</em></small> <br />
-      {props.notes}
-      
-        <a href="#!" className="secondary-content"><i className="material-icons">grade</i></a>
-    </li>
-      );
-      
+    <span className="title">{props.commonName}</span> <small><em>{props.name}</em></small> <br />
+    {props.notes}
+
+    <a href="#modal1" className="secondary-content waves-effect waves-light btn modal-trigger" onClick={props.onClick}><i className="material-icons">grade</i></a>
+
+
+  </li>
+
+
+);
+
 export default PlantCard;
--- controllers/plantsController.js
+++ controllers/plantsController.js
@@ -8,5 +8,11 @@ module.exports = {
       .sort({ date: -1 })
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
+  },
+  findBySearch: function(req, res) {
+    db.Plant
+      .find({ commonName: req.body.commonName })
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
   }
 };
