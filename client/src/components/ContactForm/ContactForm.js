e3d5f10609053e6e1a11e2bcd1755247a3c4cf8d
Samantha McCall <samanthaadams.biz@gmail.com>
2018-07-07 11:42:56 -0500

    resubmitting all changes from closed pull

--- client/src/components/ContactForm/ContactForm.js
+++ client/src/components/ContactForm/ContactForm.js
@@ -12,7 +12,7 @@ class ContactForm extends Component {
     return (
     <div>
       <h1 className="contact">Bee-friendly gardening is a breeze!</h1>
-      <h2 className="contact">Have a question? Let us help!</h2>
+      <h2 className="contact">Have a question or suggestion? Share it!</h2>
     <form className="input-field col s9" action="mailto:samanthamccallfp18@gmail.com" method="post" enctype="text/plain">
     <h3 className="contactForm contact">Topic</h3>
               <input 
@@ -24,22 +24,13 @@ class ContactForm extends Component {
               placeholder="Example: Plant identification"
               />
         {/* new field */}
-        <h3 className="col s12 contact">Post your question</h3>
+        <h3 className="contactForm contact">Tell us what's on your mind</h3>
               <textarea
               value={this.state.body.text}
               onChange={this.handleInputChange}
               type="text"
               name="body"
-              placeholder="If you're asking about a specific plant 
-              and you have a picture, upload it below."
-              />
-        {/* new field */}
-        <h3 className="input-field col s12 contact">Select a file (.pdf, .jpg, .png, or .gif):</h3> 
-              <input
-              className="contactForm"              
-              type="file"
-              onChange={this.handleInputChange}
-              name="image"
+              placeholder="Share a story, tell us about successes luring bees to your garden ... we want to hear!"
               />
         {/* new field */}
               <button
@@ -49,7 +40,7 @@ class ContactForm extends Component {
                 value="Submit"
                 onClick={this.handleFormSubmit}
                 >
-                Submit your question
+                Share your story
               </button>
       </form>
   </div>
