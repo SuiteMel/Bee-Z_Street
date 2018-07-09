878e3b72f0ee2285fa1624c8a2a1510b5e1829ce
Jennifer Lang <jenniferlang1921@gmail.com>
2018-07-09 12:04:34 -0500

    fixed conflict

--- client/src/components/PlantCard/PlantCard.js
+++ client/src/components/PlantCard/PlantCard.js
@@ -1,22 +1,4 @@
 import React from "react";
-<<<<<<< HEAD
-
-
-// import "./Main.css";
-
-const PlantCard = props => (
- 
-  <li className="collection-item avatar hoverable">
-    <img src={props.image} alt="plantcard" className="circle" height="100" width="100"/>
-    <span className="title">{props.commonName}</span> <small><em>{props.name}</em></small> <br />
-    {props.notes}
-
-    {props.children}
-  </li>
- 
-
-
-=======
 import { Link } from "react-router-dom";
 import "./PlantCard.css";
 
@@ -28,7 +10,6 @@ const PlantCard = props => (
     <p>{props.notes}</p>
   </li>
   </Link>
->>>>>>> 760b59c0ab028601331a6a3844f917663c632707
 );
 
 export default PlantCard;
