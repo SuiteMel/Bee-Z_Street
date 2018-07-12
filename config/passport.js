6bf6fecf663c5e8f091f7bd1859d75d5163ca42b
Patrick Kearney <pckearney@yahoo.com>
2018-07-12 10:55:32 -0500

    small one-line change to the passport  config file

--- config/passport.js
+++ config/passport.js
@@ -11,7 +11,7 @@ module.exports = function(passport) {
   opts.secretOrKey = settings.secret;
   passport.use(
     new JwtStrategy(opts, function(jwt_payload, done) {
-      User.findOne({ id: jwt_payload.id }, function(err, user) {
+      User.findOne({ _id: jwt_payload._id }, function(err, user) {
         if (err) {
           return done(err, false);
         }
